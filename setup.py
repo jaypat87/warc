5ba07671712e387ff6887f908e183ae80c859d94
Jay M. Patel <jaypatel87@gmail.com>
2020-02-19 10:49:02 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="warc",
+    version="0.2.1",
+    description="Python library to work with ARC and WARC files",
+    long_description=open('Readme.rst').read(),
+    license='GPLv2',
+    author="Internet Archive",
+    author_email="info@archive.org",
+    url="http://github.com/internetarchive/warc",
+    packages=["warc"],
+    platforms=["any"],
+    package_data={'': ["LICENSE", "Readme.rst"]},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
